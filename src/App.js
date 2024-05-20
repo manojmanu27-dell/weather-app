c0720b2d1cbf2a7a4995168f5a24730746cbebe4
manoj <manojthiwari890@gmail.com>
2024-05-20 13:33:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import Home from "./assets/";
+// import navigation from "./assets/location-icon.svg";
+import { useState } from "react";
+import earth from "./assets/test.png";
+import "./App.css";
+import cities from "./cities.json";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+// import location-icon.svg from './'
 function App() {
+  const [dropDown, setDropdown] = useState(false);
+  const [cityName, setCityName] = useState("");
+  const [cityList, setCityList] = useState([]);
+  // const [placeHolder, setPlaceHolder] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <nav>
+        <h1 className="heading">Weather App</h1>
+      </nav>
+      <div className="container">
+        <div className="search-bar">
+          <div className="input-group">
+            <span className="input-group-text" id="basic-addon1">
+              <img src={earth}></img>
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter you City Here"
+              aria-label="Username"
+              value={cityName}
+              onChange={(e) => handleInputChange(e)}
+            />
+          </div>
+          {dropDown && cityList.length > 0 && (
+            <div className="dropDown">
+              <ul className="list-group">
+                {cityList.map((v) => {
+                  return (
+                    <li
+                      className="list-group-item"
+                      key={v.id}
+                      aria-current="true"
+                      onClick={() => selectCityObj(v)}
+                    >
+                      {v.name + "," + v.state}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+      {/* <Home /> */}
     </div>
   );
+
+  function selectCityObj(obj) {
+    console.log("the object is", obj);
+    setCityName(obj.name + "," + obj.state);
+    setCityList([]);
+    setDropdown(false);
+  }
+  function handleInputChange(e) {
+    setCityName(e.target.value);
+    if (cityName.length >= 2) {
+      setDropdown(true);
+    } else {
+      setDropdown(false);
+    }
+    if (dropDown) {
+      let matchingCityList = cities.filter((obj) =>
+        obj.name.toLowerCase().includes(cityName.toLowerCase())
+      );
+      console.log("the city list is", matchingCityList);
+      setCityList(matchingCityList);
+    } else {
+      setCityList([]);
+    }
+  }
+  function test() {
+    setDropdown(!dropDown);
+  }
 }
+
+function getBestPlaces() {
+  console.log("the places are", cities);
+}
+
+async function run() {
+  // For text-only input, use the gemini-pro model
+
+  const genAI = new GoogleGenerativeAI(
+    "AIzaSyBLL2wllfkbbCl4LE3_HotOLkPdx07b2aA"
+  );
+
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  const prompt = "Best places to visit in Anantapur,Andhra Pradesh.";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+// run();
 
 export default App;
---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+import './home.css'
+export default function Home() {
+  return <div className="container-body">
+    <h1>test</h1>
+    <div className="card">
+        tets
+    </div>
+  </div>;
+}
